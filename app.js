89d03f9c9e82c55f9e0b39a1a6fc5edd253b8935
railnerd <railnerd@me.com>
2013-01-21 22:14:30 -0800

    make nce.js a module; refactor test code to app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+var NCE = require('./nce').NCE,
+	hexy = require('hexy');
+
+var cmdStation = new NCE("/dev/cu.SLAB_USBtoUART", function (err) {
+	if (err !== undefined) {
+		console.error("Failed to initialize: " + err);
+		process.exit(1);
+	}
+});
+
+// debugging hooks to examine command station traffic
+
+function hexDump(buf) {
+	var dumpString = hexy.hexy(buf,{numbering:"none", format:"twos", annotate: "none"});
+	return dumpString.substring(0,dumpString.length-2);
+}
+
+cmdStation.on('RECV', function (data) {
+    console.log("RECV : " + hexDump(data));
+});
+
+cmdStation.on('SEND', function (data) {
+    console.log("SEND : " + hexDump(data));
+});
+
+
+cmdStation.on('ready', function () {
+	console.log("hello, world.");
+
+	cmdStation.getVersion(function(vers) {
+		console.log("version response: "+ hexDump(vers));
+
+		cmdStation.nop();
+	});
+});
--- nce.js
+++ nce.js
@@ -3,7 +3,6 @@ var EventEmitter = require('events').EventEmitter,
 	SerialPort = require('serialport').SerialPort,
 	hexy = require('hexy');
 
-
 var	NCE = function(devicePath, callback) {
 	var self = this;
 	EventEmitter.call(self);
@@ -61,38 +60,6 @@ NCE.prototype.nop = function(callback) {
 	this.issueCommand(new Buffer([0x80]),1,callback);
 };
 
-
-
-var cmdStation = new NCE("/dev/cu.SLAB_USBtoUART", function (err) {
-	if (err !== undefined) {
-		console.error("Failed to initialize: " + err);
-		process.exit(1);
-	}
-});
-
-// debugging hooks to examine command station traffic
-
-function hexDump(buf) {
-	var dumpString = hexy.hexy(buf,{numbering:"none", format:"twos", annotate: "none"});
-	return dumpString.substring(0,dumpString.length-2);
-}
-
-cmdStation.on('RECV', function (data) {
-    console.log("RECV : " + hexDump(data));
-});
-
-cmdStation.on('SEND', function (data) {
-    console.log("SEND : " + hexDump(data));
-});
-
-
-cmdStation.on('ready', function () {
-	console.log("hello, world.");
-
-	cmdStation.getVersion(function(vers) {
-		console.log("version response: "+ hexDump(vers));
-
-		cmdStation.nop();
-	});
-});
-
+module.exports = {
+    NCE: NCE
+};
